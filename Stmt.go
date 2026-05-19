a3df3b6ee13dfc77d53090259b83569f54a0cfa8
Aayush17sty <aayush17sty@gmail.com>
2026-05-19 20:23:20 +0530

    changed logics and fixed bugs

--- Stmt.go
+++ Stmt.go
@@ -6,27 +6,51 @@ import (
 	"strconv"
 )
 
-func Expression(x ast.Expr) string {
+func Expression(x ast.Expr, m map[any]any) string {
 	switch y := x.(type) {
 	case *ast.Ident:
 		return y.Name
 	case *ast.BasicLit:
 		return y.Value
 	case *ast.BinaryExpr:
-		z := BinaryExpression(y)
+		z := BinaryExpression(y, m)
 		return z
 	default:
 		return "Error"
 	}
 }
 
-func BinaryExpression(x *ast.BinaryExpr) string {
+func BinaryExpression(x *ast.BinaryExpr, m map[any]any) string {
+	var s int
 	res := 0
-	y := Expression(x.X)
-	z := Expression(x.Y)
+	y := Expression(x.X, m)
+	z := Expression(x.Y, m)
 	i, err := strconv.Atoi(y)
 	if err != nil {
-		return "Error, y is not a valid number"
+		k := m[y]
+		q, ok := k.(string)
+		if ok == false {
+			return "error in k/y"
+		}
+		s, err = strconv.Atoi(q)
+		if err != nil {
+			return "Error in s"
+		}
+		j, err := strconv.Atoi(z)
+		if err != nil {
+			return "Error, z is not a valid number"
+		}
+		if x.Op == token.ADD {
+			res = s + j
+		}
+		if x.Op == token.SUB {
+			res = s - j
+		}
+		if x.Op == token.ASSIGN {
+			res = s
+		}
+		p := strconv.Itoa(res)
+		return p
 	}
 	j, err := strconv.Atoi(z)
 	if err != nil {
@@ -38,6 +62,23 @@ func BinaryExpression(x *ast.BinaryExpr) string {
 	if x.Op == token.SUB {
 		res = i - j
 	}
-	s := strconv.Itoa(res)
-	return s
+	if x.Op == token.ASSIGN {
+		res = i
+	}
+	p := strconv.Itoa(res)
+	return p
+}
+
+func ForStatement(x *ast.ForStmt, m map[any]any) string {
+	return x.Init.(*ast.AssignStmt).Rhs[0].(*ast.BasicLit).Value
+}
+
+func AssignmentStatement(x *ast.AssignStmt, m map[any]any) []string {
+	var result []string
+	for lhs := range x.Lhs {
+		rhs := x.Rhs[lhs]
+		v := Expression(rhs, m)
+		result = append(result, v)
+	}
+	return result
 }
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 )
 
 func main() {
+	var m = make(map[any]any)
 	src, err := os.ReadFile("example.txt")
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -22,31 +23,35 @@ func main() {
 		log.Fatal(err)
 	}
 
-	m := make(map[any]any)
-
 	ast.Inspect(node, func(node ast.Node) bool {
 
 		switch x := node.(type) {
-		case *ast.DeclStmt:
-			m[x.Decl] = nil
+		// case *ast.DeclStmt:
+		// 	m[x.Decl] = nil
+		// case *ast.ForStmt:
+		// 	m[x.Init.(*ast.AssignStmt).Lhs[0].(*ast.Ident).Name] = x.Init.(*ast.AssignStmt).Rhs[0].(*ast.BasicLit).Value
+		// 	for st := range x.Body.List {
+		// 		switch s := x.Body.List[st].(type) {
+		// 		case *ast.AssignStmt:
+		// 			m[s.Lhs[0].(*ast.Ident).Name] = s.Rhs[0].(*ast.BinaryExpr).X.(*ast.Ident).Name + s.Rhs[0].(*ast.BinaryExpr).Y.(*ast.BasicLit).Value
+		// 		}
+		// 	}oop body logic
+		// case *ast.BinaryExpr:
+		// 	if x.Op == token.ASSIGN {
+		// 		m[x.X.(*ast.Ident).Name] = x.Y.(*ast.BasicLit).Value
+		// 	}
+		// case *ast.AssignStmt:
+		// 	for lhs := range x.Lhs {
+		// 		m[x.Lhs[lhs].(*ast.Ident).Name] = x.Rhs[lhs].(*ast.BasicLit).Value
+		// 	}
+		// }
+		case ast.Expr:
+			v := Expression(x, m)
+			m[x] = v
 		case *ast.ForStmt:
-			m[x.Init.(*ast.AssignStmt).Lhs[0].(*ast.Ident).Name] = x.Init.(*ast.AssignStmt).Rhs[0].(*ast.BasicLit).Value
-			for st := range x.Body.List {
-				switch s := x.Body.List[st].(type) {
-				case *ast.AssignStmt:
-					m[s.Lhs[0].(*ast.Ident).Name] = s.Rhs[0].(*ast.BinaryExpr).X.(*ast.Ident).Name + s.Rhs[0].(*ast.BinaryExpr).Y.(*ast.BasicLit).Value
-				}
-			}
-		case *ast.BinaryExpr:
-			if x.Op == token.ASSIGN {
-				m[x.X.(*ast.Ident).Name] = x.Y.(*ast.BasicLit).Value
-			}
-		case *ast.AssignStmt:
-			for lhs := range x.Lhs {
-				m[x.Lhs[lhs].(*ast.Ident).Name] = x.Rhs[lhs].(*ast.BasicLit).Value
-			}
+			v := ForStatement(x, m)
+			m[x] = v
 		}
-
 		return true
 
 	})
